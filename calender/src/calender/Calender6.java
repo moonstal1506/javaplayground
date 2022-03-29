a655df8f7077bd0c360fb9ed92c4b8b865b80cd3
moonstal1506 <moonstal1506@gmail.com>
2022-03-30 02:04:22 +0900

    운년,평년

--- calender/src/calender/Calender6.java
+++ calender/src/calender/Calender6.java
@@ -3,24 +3,37 @@ package calender;
 import java.util.Scanner;
 
 public class Calender6 {
-	
-	private static final String PROMPT="cal> ";
+
+	private static final String PROMPT = "cal> ";
 
 	private static final int[] DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private static final int[] YDAYS = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
-	public static int day(int month) {
+	public static boolean isyday(int year) {
+		if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+			return true;
+		}
+		return false;
+	}
+
+	public static int day(int year, int month) {
+		if(isyday(year)) {
+			return YDAYS[month - 1];
+		}
 		return DAYS[month - 1];
 	}
+
 	public void printCal(int year, int month) {
-		int maxday=day(month);
-		System.out.printf("%4d년 %3d월\n",year,month);
+		int maxday = day(year,month);
+		System.out.printf("%4d년 %3d월\n", year, month);
 		System.out.printf(" su mo tu we th fr sa");
 		System.out.println();
 		System.out.println("--------------------");
-		for(int i=1;i<=maxday;i++) {
-			System.out.printf("%3d",i);
-			if(i%7==0) System.out.println();
+		for (int i = 1; i <= maxday; i++) {
+			System.out.printf("%3d", i);
+			if (i % 7 == 0)
+				System.out.println();
 		}
 		System.out.println();
 	}
-}	
+}
